19593130e16d45cb77a2c02c186310aa272acfa2
nirzon32 <tohedulislamnirzon@gmail.com>
2024-10-25 00:05:46 +0600

    es6 module explore

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+         <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to My React App</h1>
+    </>
+  )
+}
+
+export default App
